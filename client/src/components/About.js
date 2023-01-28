3e3f71c87732720c3151931d9e337387619b39fd
akashvadnala <akashvadnala123@gmail.com>
2023-01-28 17:20:50 +0530

    about solved

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -11,14 +11,15 @@ const About = () => {
   const editor = useRef(null);
   const { user } = useContext(Context);
   const [preview, setPreview] = useState(true);
-  const [overview, setOverview] = useState();
+  const [about, setAbout] = useState("");
   const [load, setLoad] = useState(0);
   const [desc, setDesc] = useState("");
-  const getOverview = () => {
+  const getAbout = () => {
     axios.get(`${SERVER_URL}/getAbout/`).then((data) => {
       if (data.status === 200) {
-        setOverview(data.data);
+        setAbout(data.data);
         setDesc(data.data.about);
+        console.log('about',data.data);
         setLoad(1);
       } else {
         setLoad(-1);
@@ -26,12 +27,12 @@ const About = () => {
     });
   };
   useEffect(() => {
-    getOverview();
+    getAbout();
     setPreview(true);
   }, [user]);
 
   const handleValue = (value) => {
-    setOverview({ ...overview, ["about"]: value });
+    setAbout({...about,['about']:value});
   };
   const showPreview = () => {
     setPreview(true);
@@ -40,12 +41,14 @@ const About = () => {
     setPreview(false);
   };
   const cancelIt = () => {
-    setOverview({ ...overview, ["about"]: desc });
+    setAbout({...about,['about']:desc});
     setPreview(true);
   };
   const saveIt = () => {
-    console.log("before saving", overview);
-    axios.put(`${SERVER_URL}/updateAbout`, overview, {
+    console.log("before saving", about);
+    axios.put(`${SERVER_URL}/updateAbout/${about._id}`,
+    about, 
+    {
       headers: { "Content-Type": "application/json" },
     });
     setPreview(true);
@@ -56,87 +59,70 @@ const About = () => {
       {load === 0 ? (
         <Loading />
       ) : load === 1 ? (
-        <div className="compete-container">
+        <div className="about-container">
           <div className="adjust" style={{ overflow: "hidden" }}>
-            <div className="overview-container">
-              <div className="border">
-                <div className="row p-3 pb-1">
-                  <div className="col-sm-8">
-                    <h5>
-                      <strong>About</strong>
-                    </h5>
-                  </div>
-                  <div className="col-sm-4 text-end">
-                    {user ? (
-                      user.isadmin && preview ? (
-                        <>
-                          <button
-                            className="btn btn-outline-dark btn-sm mx-1"
-                            onClick={showEdit}
-                          >
-                            Edit
-                          </button>
-                          <button
-                            className="btn btn-outline-dark btn-sm mx-1"
-                            onClick={saveIt}
-                          >
-                            Save
-                          </button>
-                        </>
-                      ) : (
-                        <>
-                          <button
-                            className="btn btn-outline-dark btn-sm mx-1"
-                            onClick={showPreview}
-                          >
-                            Preview
-                          </button>
-                          <button
-                            className="btn btn-outline-dark btn-sm mx-1"
-                            onClick={cancelIt}
-                          >
-                            Cancel
-                          </button>
-                          <button
-                            className="btn btn-outline-dark btn-sm mx-1"
-                            onClick={saveIt}
-                          >
-                            Save
-                          </button>
-                        </>
-                      )
-                    ) : null}
-                  </div>
-                </div>
-                {preview ? (
-                  <div className="p-3">
-                    <p dangerouslySetInnerHTML={{ __html: overview.about }}></p>
-                  </div>
-                ) : (
-                  <JoditEditor
-                    name="content"
-                    ref={editor}
-                    value={overview.about}
-                    onChange={handleValue}
-                  />
-                )}
-                <div className="p-3">
-                  <h5>
-                    <strong>About</strong>
-                  </h5>
-                  <div>
-                    <iframe
-                      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3912.179525211182!2d75.93149181532856!3d11.321585291953028!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba642e2bb691caf%3A0xe14dc0a5771fd2ce!2sCentral%20Computer%20Center!5e0!3m2!1sen!2sin!4v1674127152405!5m2!1sen!2sin"
-                      width="100%"
-                      height="600"
-                      style={{border:"0"}}
-                      allowFullScreen=""
-                      loading="lazy"
-                      referrerPolicy="no-referrer-when-downgrade"
-                    ></iframe>
-                  </div>
-                </div>
+            <div className="row p-3 pb-1">
+              <div className="col-sm-8">
+                <h2>About</h2>
               </div>
+              <div className="col-sm-4 text-end">
+                {user ? (
+                  user.isadmin && preview ? (
+                    <>
+                      <button
+                        className="btn btn-outline-dark btn-sm mx-1"
+                        onClick={showEdit}
+                      >
+                        Edit
+                      </button>
+                    </>
+                  ) : (
+                    <>
+                      <button
+                        className="btn btn-outline-dark btn-sm mx-1"
+                        onClick={showPreview}
+                      >
+                        Preview
+                      </button>
+                      <button
+                        className="btn btn-outline-dark btn-sm mx-1"
+                        onClick={cancelIt}
+                      >
+                        Cancel
+                      </button>
+                      <button
+                        className="btn btn-outline-dark btn-sm mx-1"
+                        onClick={saveIt}
+                      >
+                        Save
+                      </button>
+                    </>
+                  )
+                ) : null}
+              </div>
+            </div>
+            {preview ? (
+              <div className="p-3">
+                <p dangerouslySetInnerHTML={{ __html: about.about }}></p>
+              </div>
+            ) : (
+              <JoditEditor
+                name="content"
+                ref={editor}
+                value={about.about}
+                onChange={handleValue}
+              />
+            )}
+            <div className="p-3">
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3912.179525211182!2d75.93149181532856!3d11.321585291953028!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba642e2bb691caf%3A0xe14dc0a5771fd2ce!2sCentral%20Computer%20Center!5e0!3m2!1sen!2sin!4v1674127152405!5m2!1sen!2sin"
+                width="100%"
+                height="600"
+                style={{ border: "0" }}
+                allowFullScreen=""
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
+              ></iframe>
             </div>
           </div>
         </div>
--- server/router/about.js
+++ server/router/about.js
@@ -1,22 +1,25 @@
 const express = require('express');
 const router = express.Router();
 const About = require('../model/AboutSchema');
-require('../db/conn');
 
-router.route('/updateAbout').put(async (req,res) => {
+router.route('/updateAbout/:id').put(async (req,res) => {
     try{
-        const updatedAbout = await About.findOne({});
-        updatedAbout.about = req.body.about;
-        await updatedAbout.save();
-        res.status(200).json(updatedAbout);
+        console.log('body',req.body);
+        const updatedAbout = await About.findByIdAndUpdate(req.params.id,req.body,{
+            new:true
+        });
     }catch (err) {
-        res.status(422).json(err);
+        console.log(err);
     }
-})
+});
 
 router.route('/getAbout').get(async (req,res) => {
-    const aboutData = await About.find({});
-    res.status(200).json(aboutData[0]);
+    const aboutData = await About.findOne({});
+    if(!aboutData){
+        const about = new About();
+        return res.status(200).json(about);
+    }
+    res.status(200).json(aboutData);
 });
 
 module.exports = router;
