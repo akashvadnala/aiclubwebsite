ccac63d1fe4cb81da006d83f7a324d7c08b30f70
BhanuPrakashPebbeti <pebbetibhanu2017@gmail.com>
2023-01-29 19:13:43 +0530

    Added alerts

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,12 +1,15 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import axios from 'axios';
+import { NavLink } from "react-router-dom";
 import './Footer.css';
 import { CLIENT_URL, SERVER_URL } from '../../EditableStuff/Config';
+import {alertContext} from "../../Context/Alert";
 
 const Footer = () => {
 
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
+  const { showAlert } = useContext(alertContext);
 
   const addSubscriber = async (e) => {
     e.preventDefault();
@@ -22,6 +25,7 @@ const Footer = () => {
     }catch(error){
       console.log(error);
     }
+    showAlert("Subscribed Successfully","success");
     setName('');
     setEmail('');
     return;
@@ -31,17 +35,40 @@ const Footer = () => {
     <div className='footer' id='contactus'>
       <div className='adjust'>
         <div className='row'>
-          <div className='col-md-4 col-sm-12'>
+          <div className='col-md-2 col-sm-12'>
             <h5>AI CLUB</h5>
-            <p>Our Team</p>
-            <p>Projects</p>
-            <p>Blogs</p>
-            <p>About</p>
+            <NavLink to="/team" style={{color:"#212529"}}><p>Our Team</p></NavLink>
+            <NavLink to="/projects" style={{color:"#212529"}}><p>Projects</p></NavLink>
+            <NavLink to="/blogs" style={{color:"#212529"}}><p>Blogs</p></NavLink>
+            <NavLink to="/events" style={{color:"#212529"}}><p>Events</p></NavLink>
+          </div>
+          <div className='col-md-2 col-sm-12'>
+            <h5>&nbsp;&nbsp;</h5>
+            <NavLink to="/gallery" style={{color:"#212529"}}><p>Gallery</p></NavLink>
+            <NavLink to="/about" style={{color:"#212529"}}><p>About</p></NavLink>
           </div>
           <div className='col-md-4 col-sm-12'>
             <h5>CONTACT US</h5>
             <p>Address: <span>Central Computer Center, NIT Calicut, Kozhikode, Kerala - 673601</span></p>
-            <p>Email: <span>aiclub@nitc.ac.in</span></p>
+            <div className="d-flex justify-content-start my-4">
+            <a
+              className="fas fa-envelope fa-lg mx-2"
+              style={{ color: "#55acee" }}
+              href={`mailto:aiclub@nitc.ac.in`}
+            ></a>
+            <a
+              className="fab fa-linkedin-in fa-lg mx-2"
+              style={{ color: "#3b5998" }}
+              href="https://in.linkedin.com/company/ai-club-nitc"
+              target = "_blank"
+            ></a>
+            <a
+              className="fab fa-github fa-lg mx-2"
+              style={{ color: "#333333" }}
+              href="https://github.com/AI-Club-at-NITC"
+              target = "_blank"
+            ></a>
+          </div>
           </div>
           <div className='col-md-4 col-sm-12'>
             <h5>LATEST UPDATES</h5>
