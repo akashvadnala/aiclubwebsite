fbdef5e32eada21218c75dc494abb7d4a220dd7d
BhanuPrakashPebbeti <pebbetibhanu2017@gmail.com>
2023-02-17 03:40:24 +0530

    Adding dockerfiles

--- server/Config.js
+++ server/Config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    CLIENT_URL: process.env.CLIENT_URL || "http://localhost:3000",
-    DATABASE: process.env.DATABASE || 'mongodb://127.0.0.1:27017/aiclub',
+    CLIENT_URL: process.env.CLIENT_URL || "http://localhost:1337",
+    DATABASE: process.env.DATABASE || 'mongodb://aiclubMongo:27017/aiclub',
 } 
