214dddd7b14c77c67068c31c5df7ce7088d49f80
BhanuPrakashPebbeti <pebbetibhanu2017@gmail.com>
2023-02-18 02:32:39 +0530

    Added docker setup

--- server/Config.js
+++ server/Config.js
@@ -1,4 +1,6 @@
 module.exports = {
-    CLIENT_URL: process.env.CLIENT_URL || "http://localhost:1337",
-    DATABASE: process.env.DATABASE || 'mongodb://aiclubMongo:27017/aiclub',
+    PORT: process.env.PORT || 5000,
+    CLIENT_URL: process.env.CLIENT_URL || "http://localhost:3000",
+    SERVER_URL: process.env.SERVER_URL || "http://localhost:5000/api/v1",
+    DATABASE: process.env.DATABASE || 'mongodb://localhost:27017/aiclub',
 } 
